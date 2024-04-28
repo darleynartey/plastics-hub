01d7706c3b885318d966c6ee181aaa3000ef6a50
darleynartey <barbaradnartey@gmail.com>
2024-04-28 23:29:04 +0000

    Create sidebar

--- src/pages/dealerdashboard/components/SideBar.jsx
+++ src/pages/dealerdashboard/components/SideBar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+// import React from 'react'
+// import { HomeOutlinedIcon } from 'react-icons'
 
-const SideBar = () => {
-  return (
-    <div>
+// const SideBar = () => {
+//   return (
+//     <div className="sidebar"> 
+//       <div className="logo">
+//         <h1 className="logo-icon">PLASTICS_HUB</h1>
+//         <h5 className="admin">Admin Dashboard</h5>
+//       </div>
+//       <div className="menu-bar">
+//         <ul>
+//           <li>
+//             <HomeOutlinedIcon fontSize="small" />
+//             <span onClick={() => navigate("/")}>Dashboard</span>
+//           </li>
+//           <li>
+//             <DescriptionOutlinedIcon fontSize="small" />
+//               <span onClick={() => navigate("/ridedata")}>Inbox</span>
+//           </li>
+//           <li>
+//             <AirportShuttleOutlinedIcon fontSize="small" />
+//               <span onClick={() => navigate("/operational")}>Profile</span>
+//           </li>
+//           <li>
+//             <GroupOutlinedIcon fontSize="small" />
+//               <span onClick={() => navigate("/useranalysis")}>Settings</span>
+//           </li>
+          
+//         </ul>
+//       </div>
 
-    </div>
-  )
-}
+//       <footer id="footer">
+//         <h5>Admin Dashboard</h5>
+//         <h6>© 2024 All Rights Reserved</h6>
+//       </footer>
+//     </div>
+//   )
+// }
 
-export default SideBar
+// export default SideBar
